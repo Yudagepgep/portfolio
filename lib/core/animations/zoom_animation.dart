058dd4535a2811b1d489801fd135f97c026d628e
yuda <yudagepgep25@gmail.com>
2024-04-09 02:29:01 +0700

    update foto animatio ke saya.jpg

--- lib/core/animations/zoom_animation.dart
+++ lib/core/animations/zoom_animation.dart
@@ -92,7 +92,7 @@ class _ZoomAnimationsState extends State<ZoomAnimations>
               image: const DecorationImage(
                 fit: BoxFit.cover,
                 alignment: Alignment.bottomLeft,
-                image: AssetImage('assets/imgs/yuda.jpg'),
+                image: AssetImage('assets/imgs/saya.jpg'),
               ),
             ),
           ),
